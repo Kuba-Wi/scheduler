600fe1f317b531973485fbcc5f84ba5ee2779873
kuba <winczukjakub@gmail.com>
2022-02-01 00:00:37 +0100

    removed magic number from round robin test

--- test/test_roundRobin.py
+++ test/test_roundRobin.py
@@ -6,17 +6,19 @@ import test.helpers as h
 class TestRoundRobin(h.TestHelper):
     def test_empty_line(self):
         PROCESSORS_NUM = 1
+        TIME_QUANTUM = 1
         input = io.StringIO("\n")
-        strategy = rr.RoundRobin(PROCESSORS_NUM, 1, input)
+        strategy = rr.RoundRobin(PROCESSORS_NUM, TIME_QUANTUM, input)
 
         self.check_next_line(strategy, [-1])
 
     def test_one_processor_2procs(self):
         PROCESSORS_NUM = 1
+        TIME_QUANTUM = 1
         input = io.StringIO("0 1 0 2\n" +
                             "1 2 0 1\n")
 
-        strategy = rr.RoundRobin(PROCESSORS_NUM, 1, input)
+        strategy = rr.RoundRobin(PROCESSORS_NUM, TIME_QUANTUM, input)
 
         self.check_next_line(strategy, [1])
         self.check_next_line(strategy, [2])
@@ -24,23 +26,26 @@ class TestRoundRobin(h.TestHelper):
 
     def test_one_processor_3procs(self):
         PROCESSORS_NUM = 1
-        input = io.StringIO("0 1 0 2 2 0 2\n" +
-                            "1 3 0 1\n")
+        TIME_QUANTUM = 2
+        input = io.StringIO("0 1 0 1 2 0 3\n" +
+                            "1 3 0 2\n")
 
-        strategy = rr.RoundRobin(PROCESSORS_NUM, 1, input)
+        strategy = rr.RoundRobin(PROCESSORS_NUM, TIME_QUANTUM, input)
 
         self.check_next_line(strategy, [1])
         self.check_next_line(strategy, [2])
+        self.check_next_line(strategy, [2])
         self.check_next_line(strategy, [3])
-        self.check_next_line(strategy, [1])
+        self.check_next_line(strategy, [3])
         self.check_next_line(strategy, [2])
 
     def test_two_processors_4procs(self):
         PROCESSORS_NUM = 2
+        TIME_QUANTUM = 1
         input = io.StringIO("0 1 0 2 2 0 2 3 0 1\n" +
                             "1 4 0 1\n")
 
-        strategy = rr.RoundRobin(PROCESSORS_NUM, 1, input)
+        strategy = rr.RoundRobin(PROCESSORS_NUM, TIME_QUANTUM, input)
 
         self.check_next_line(strategy, [1, 2])
         self.check_next_line(strategy, [3, 4])
@@ -48,8 +53,9 @@ class TestRoundRobin(h.TestHelper):
 
     def test_three_processors_1proc(self):
         PROCESSORS_NUM = 3
+        TIME_QUANTUM = 1
         input = io.StringIO("0 1 0 1\n")
 
-        strategy = rr.RoundRobin(PROCESSORS_NUM, 1, input)
+        strategy = rr.RoundRobin(PROCESSORS_NUM, TIME_QUANTUM, input)
 
         self.check_next_line(strategy, [1, -1, -1])
